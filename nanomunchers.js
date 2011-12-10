092437c3db56cce3e64c8a2621eb0754dc3012f4
Brandon Reiss <brandon.l.reiss@gmail.com>
2011-12-10 00:35:24 -0500

    Motion using timers. Would look cool as thrusters, but will do for now.

--- nanomunchers.js
+++ nanomunchers.js
@@ -12,54 +12,101 @@ var GameUI = {
                 this.player1 = new Mothership(this.paper.circle(100,100,50))
                 this.player2 = new Mothership(this.paper.circle(300,300,50))
 
-                $(document).keypress(this.onKeyDown.bind(this));
+                $(document).keydown(this.onKey.bind(this));
+                $(document).keyup(this.onKey.bind(this));
               },
-  onKeyDown: function(e){
+
+  onKey: function(e){
                // Dispatch WASD to move player 1's ship, and spacebar to drop
                // Arrow keys for player 2's ship, and enter to drop
-               var keycode = e.charCode;
-               if(keycode >= KEYCODES.A + KEYCODES.CAPS_OFFSET &&
-                   keycode <= KEYCODES.Z + KEYCODES.CAPS_OFFSET)
-                 keycode -= KEYCODES.CAPS_OFFSET;
+               var keyCode = e.keyCode;
+               if(keyCode >= KEYCODES.A + KEYCODES.CAPS_OFFSET &&
+                   keyCode <= KEYCODES.Z + KEYCODES.CAPS_OFFSET){
+                 keyCode -= KEYCODES.CAPS_OFFSET;
+               }
 
-               var keyName = KEYCODES[keycode]
+               var keyName = KEYCODES[keyCode]
                if(this.keyMappings[keyName] !== undefined){
-                 this.keyMappings[keyName].apply(this);
+                 this.keyMappings[keyName].apply(this, [e.type]);
                }
              },
 
-  keyMappings: { "W":        function(){this.player1.up()},
-                 "A":        function(){this.player1.left()},
-                 "S":        function(){this.player1.down()},
-                 "D":        function(){this.player1.right()},
-                 "SPACEBAR": function(){this.player1.drop()},
-                 "O":       function(){this.player2.up()},
-                 "K":     function(){this.player2.left()},
-                 "SEMICOLON":    function(){this.player2.right()},
-                 "L":     function(){this.player2.down()},
-                 "RETURN":   function(){this.player2.drop()}}
+  keyMappings: { // Player 1.
+                 "W":         function(flag){this.player1.move(flag, "UP")},
+                 "A":         function(flag){this.player1.move(flag, "LEFT")},
+                 "S":         function(flag){this.player1.move(flag, "DOWN")},
+                 "D":         function(flag){this.player1.move(flag, "RIGHT")},
+                 "SPACEBAR":  function(flag){this.player1.drop(flag)},
+                 // Player 2.
+                 "O":         function(flag){this.player2.move(flag, "UP")},
+                 "K":         function(flag){this.player2.move(flag, "LEFT")},
+                 "SEMICOLON": function(flag){this.player2.move(flag, "RIGHT")},
+                 "L":         function(flag){this.player2.move(flag, "DOWN")},
+                 "RETURN":    function(flag){this.player2.drop(flag)}}
 
-                 //"UP":       function(){console.log(this)
-                  // this.player2.up()},
+                 //"UP":       function(){this.player2.up()},
                  //"DOWN":     function(){this.player2.down()},
                  //"LEFT":     function(){this.player2.left()},
                  //"RIGHT":    function(){this.player2.right()},
 }
 
 var Mothership = function(canvasElement){
+  var TIMER_UPDATE_MS = 50;
   this.canvasElement = canvasElement;
-  this.animationTime = 50;
-  this.up = function(){
-    this.canvasElement.animate({cy: this.canvasElement.attrs.cy-5}, this.animationTime)
+  this.animationTime = 25;
+  this.keysDown = {"UP": 0, "DOWN": 0, "LEFT": 0, "RIGHT": 0};
+  this.keyDownCount = 0;
+  this.timer = null;
+  this.onKeyDownTimer = function(){
+    if(this.keysDown["UP"] && !this.keysDown["DOWN"]){
+      this.canvasElement.animate({cy: this.canvasElement.attrs.cy-5},
+                                 this.animationTime)
+    }
+    if(this.keysDown["DOWN"] && !this.keysDown["UP"]){
+      this.canvasElement.animate({cy: this.canvasElement.attrs.cy+5},
+                                 this.animationTime)
+    }
+    if(this.keysDown["LEFT"] && !this.keysDown["RIGHT"]){
+      this.canvasElement.animate({cx: this.canvasElement.attrs.cx-5},
+                                 this.animationTime)
+    }
+    if(this.keysDown["RIGHT"] && !this.keysDown["LEFT"]){
+      this.canvasElement.animate({cx: this.canvasElement.attrs.cx+5},
+                                 this.animationTime)
+    }
   }.bind(this)
-  this.right= function(){
-    this.canvasElement.animate({cx: this.canvasElement.attrs.cx+5},this.animationTime)
+  this.move = function(flag, theKey){
+    if("keydown" === flag){
+      if(this.keysDown[theKey] === 0)
+      {
+        if(theKey === "UP"){
+          this.canvasElement.animate({cy: this.canvasElement.attrs.cy-5},
+                                     this.animationTime)
+        }
+        else if(theKey === "DOWN"){
+          this.canvasElement.animate({cy: this.canvasElement.attrs.cy+5},
+                                     this.animationTime)
+        }
+        else if(theKey === "LEFT"){
+          this.canvasElement.animate({cx: this.canvasElement.attrs.cx-5},
+                                     this.animationTime)
+        }
+        else if(theKey === "RIGHT"){
+          this.canvasElement.animate({cx: this.canvasElement.attrs.cx+5},
+                                     this.animationTime)
+        }
+        this.keysDown[theKey] = 1;
+        if(++this.keyDownCount === 1){
+          this.timer = setInterval(this.onKeyDownTimer, TIMER_UPDATE_MS);
+        }
+      }
+    }
+    else if("keyup" === flag){
+      this.keysDown[theKey] = 0;
+      if(--this.keyDownCount === 0){
+        clearInterval(this.timer);
+        this.timer = null
+      }
+    }
   }.bind(this)
-  this.left = function(){
-    this.canvasElement.animate({cx: this.canvasElement.attrs.cx-5},this.animationTime)
-  }.bind(this)
-  this.down= function(){
-    this.canvasElement.animate({cy: this.canvasElement.attrs.cy+5},this.animationTime)
-  }.bind(this)
-  this.drop = function(){}
 }

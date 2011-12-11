1f6fff9e593baed990f69ccc58e7d159f14c8076
Raphael Sofaer <rjs454@box150.cims.nyu.edu>
2011-12-11 17:29:08 -0500

    Start simulator

---
+++ simulator.js
@@ -0,0 +1,48 @@
+var Simulator = function(){
+  //1. Munchers are dropped.
+  //2. Munchers resolve conflicts.
+  //3. Munchers munch.
+  //4. Munchers move.
+  //5. Back to 1.
+  // This function assumes that all muchers have been dropped.  It runs steps 2-4
+  this.stepTime = function(board){
+    this.resolveConflicts(board);
+    this.munch(board);
+    this.move(board);
+  }
+
+  this.resolveConflicts = function(board){
+    var conflictMap = {}
+    board.munchers.forEach(function(muncher){
+      code = muncher.toS();
+      if(conflictMap[code] === undefined){
+        conflictMap[code] = [];
+      }
+      conflictMap[code].push(muncher);
+    });
+
+    for(p in conflictMap){
+      if(conflictMap.hasOwnProperty(p)){
+        var muncherArr = conflictMap(p);
+        if(muncherArr.length > 1){
+          // We have more than one muncher at a node.  We must resolve through combat.
+        }
+      }
+    }
+  }
+
+  this.munch = function(board){
+    board.munchers.forEach(function(muncher){
+      var node = board.closestNode(muncher.loc);
+      node.consume(muncher.player, muncher.player.munchColor);
+    });
+  }
+
+  this.move = function(board){
+    board.munchers.forEach(function(muncher){
+      var node = board.closestNode(muncher.loc);
+
+    });
+  }
+}()
+bindAllFunctions(Simulator);
